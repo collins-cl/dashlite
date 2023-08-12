760b4b4bd1bf8fe6c20b5075fcce6348a998abe8
Collins-cl <collinsolads@gmail.com>
2023-08-12 20:34:05 +0100

    finished working on top deals

--- src/Components/Topcategories/TopCategories.jsx
+++ src/Components/Topcategories/TopCategories.jsx
@@ -1,10 +1,86 @@
-import React from 'react'
-import "../Topcategories/Topcategories.scss"
+import React from "react";
+import "../Topcategories/Topcategories.scss";
+import { BsThreeDots } from "react-icons/bs";
+import { IconButton } from "@mui/material";
+import { Categories } from "../../Dummies/Courses";
 
 const TopCategories = () => {
-  return (
-    <div>TopCategories</div>
-  )
-}
+  const categories = Categories;
 
-export default TopCategories
+  const container = {
+    height: 7,
+    maxWidth: "100%",
+    backgroundColor: "#eee",
+    margin: 50,
+  };
+
+  const bar = {
+    height: "100%",
+    width: `${categories.progress}%`,
+    backgroundColor: "#90caf9",
+    borderRadius: "2px",
+  };
+
+  return (
+    <div className="topcategory">
+      <div className="head">
+        <div className="title">
+          <div>Top Categories</div>
+          <p>In the last 15 days buys and sells overview</p>
+        </div>
+
+        <div className="option">
+          <div className="div">
+            <IconButton>
+              <BsThreeDots />
+            </IconButton>
+          </div>
+        </div>
+      </div>
+
+      <div className="categories">
+        {categories &&
+          categories.map((data) => (
+            <div className="wrapper">
+              <div className="pro-gress">
+                <div
+                  style={{
+                    height: 7,
+                    maxWidth: "100%",
+                    backgroundColor: `${data.backgroundColor}`,
+                  }}
+                  className="container"
+                >
+                  <div
+                    style={{
+                      height: "100%",
+                      width: `${data.progress}%`,
+                      backgroundColor: `${data.color}`,
+                    }}
+                    role="progressbar"
+                    aria-valuenow={data.progress}
+                    aria-valuemin={0}
+                    aria-valuemax={100}
+                  ></div>
+                </div>
+              </div>
+
+              <div className="prog-name">
+                <span
+                  style={{
+                    background: `${data.color}`,
+                    height: 10,
+                    width: 10,
+                    borderRadius: 2,
+                  }}
+                ></span>
+                <p>{data.category}</p>
+              </div>
+            </div>
+          ))}
+      </div>
+    </div>
+  );
+};
+
+export default TopCategories;
---
+++ src/Dummies/Courses.jsx
@@ -0,0 +1,44 @@
+export const Categories = [
+  {
+    progress: 60,
+    color: "orange",
+    backgroundColor: "rgba(244, 199, 114, 0.898)",
+    category: "Web Development",
+  },
+  {
+    progress: 20,
+    color: "rgb(74, 74, 226)",
+    backgroundColor: "rgba(98, 98, 246, 0.529)",
+    category: "Mobile Application",
+  },
+  {
+    progress: 55,
+    color: "rgb(109, 232, 109)",
+    backgroundColor: "rgba(145, 248, 145, 0.808)",
+    category: "Graphics Design",
+  },
+  {
+    progress: 43,
+    color: "rgb(54, 54, 126)",
+    backgroundColor: "rgba(76, 76, 152, 0.734)",
+    category: "Database",
+  },
+  {
+    progress: 61,
+    color: "rgb(109, 232, 109)",
+    backgroundColor: "rgba(145, 248, 145, 0.808)",
+    category: "Marketing",
+  },
+  {
+    progress: 78,
+    color: "rgb(158, 55, 55)",
+    backgroundColor: "#9c5454ac",
+    category: "Machine Learning",
+  },
+  {
+    progress: 50,
+    color: "#802180",
+    backgroundColor: "#7e447eb7",
+    category: "Data Science",
+  },
+];
